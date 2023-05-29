29bb7421f5ee781278ffc3fe3ac04d744cc7232a
Marco Pacchiotti <ICTS22-24.016@studenti.local>
2023-05-29 15:29:32 +0200

    rm soloquery

--- SQLQuery3.sql
+++
@@ -1,8 +0,0 @@
-/****** Script per comando SelectTopNRows da SSMS  ******/
-insert into Regioni(id,Denominazione,IdRipartione)
-SELECT distinct [Codice_regione]
-      ,[Denominazione_regione]
-      
-      ,[Codice_ripartizione]
-      
-  FROM [Istat].[dbo].[Import]
