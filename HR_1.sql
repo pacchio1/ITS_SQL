44f2da7ab369ea813d14a0f918bd8c54a4cdda0f
Marco Pacchiotti <ICTS22-24.016@studenti.local>
2023-05-29 17:56:50 +0200

    Create HR_1.sql

---
+++ HR_1.sql
@@ -0,0 +1,28 @@
+SELECT top 1 countries.country_name, COUNT(employees.employee_id) AS num_employees
+FROM countries
+JOIN regions ON countries.region_id = regions.region_id
+JOIN locations ON countries.country_id = locations.country_id
+JOIN departments ON locations.location_id = departments.location_id
+JOIN employees ON departments.department_id = employees.department_id
+JOIN jobs ON employees.job_id = jobs.job_id
+GROUP BY countries.country_name
+ORDER BY num_employees DESC
+
+DECLARE @job VARCHAR(50);
+SET @job = 'Public Accountant';
+SELECT e.last_name, e.first_name, e.email
+FROM employees e
+JOIN jobs ON e.job_id= jobs.job_id
+JOIN departments ON e.department_id = departments.department_id
+WHERE jobs.job_title = @job;
+
+SELECT distinct departments.department_name 
+FROM employees
+JOIN departments ON employees.department_id = departments.department_id
+JOIN locations ON departments.location_id = locations.location_id
+JOIN countries ON locations.country_id = countries.country_id
+WHERE countries.region_id = 2;
+
+DECLARE @salario int;
+SET @salario = 100;
+
